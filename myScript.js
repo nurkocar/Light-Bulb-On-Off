d568aa67a02647e0912c9e9f698428942d95b7e9
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-09-30 02:33:21 +0000

    seperate html into css and js

---
+++ myScript.js
@@ -0,0 +1,19 @@
+const buttonElement = document.querySelector("#turnOnOff");
+const imgElement = document.querySelector("#bulb");
+
+function turnOnOff() {
+    
+  if (imgElement.src.includes("ONbulb")) {
+
+    imgElement.src = "./images/OFFbulb.png";
+    document.querySelector("body").bgColor = "whitesmoke";
+    buttonElement.innerHTML = "Turn On";
+
+  } else {
+
+    imgElement.src = "./images/ONbulb.png";
+    document.querySelector("body").bgColor = "black";
+    buttonElement.innerHTML = "Turn Off";
+
+  }
+}
